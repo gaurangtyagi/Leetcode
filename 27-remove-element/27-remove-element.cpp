20e3bede189544fa702fdb3789ce965ef1018910
Gaurang Tyagi <85627251+gaurangtyagi@users.noreply.github.com>
2022-05-02 12:10:39 +0530

    Time: 7 ms (15.77%), Space: 8.9 MB (37.03%) - LeetHub

---
+++ 27-remove-element/27-remove-element.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int j = 0;
+        for (int i = 0; i<nums.size(); i++)
+        {
+            if (nums[i] != val)
+            {
+                swap (nums[i], nums[j]);
+                j++;
+            }
+            else 
+            {
+                nums[i] = -1;
+            }
+        }
+        return j;
+    }
+};
