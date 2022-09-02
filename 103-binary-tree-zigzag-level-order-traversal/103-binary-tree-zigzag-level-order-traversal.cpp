67c625300cdd841aa7317ec3eccf4fa30f195e91
Gaurang Tyagi <85627251+gaurangtyagi@users.noreply.github.com>
2022-09-03 03:22:26 +0530

    Time: 0 ms (100.00%), Space: 11.9 MB (92.65%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -21,7 +21,7 @@ public:
         while(!q.empty())
         {
             int size = q.size();
-            vector<int> level;
+            vector<int> level(size);
             flag = !flag; 
             for (int i = 0; i<size; i++)
             {
@@ -29,9 +29,9 @@ public:
                 q.pop();
                 if (node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
-                level.push_back(node->val);
+                int index = (!flag) ? i: size- i - 1;
+                level[index] = (node->val);
             }
-            if (flag) reverse(level.begin(), level.end());
             ans.push_back(level);
         }
         return ans;
